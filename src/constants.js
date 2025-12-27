72f90eb8d1aeaa310711e3732719d05219f00909
L0rdxD <159791952+L0rdxD@users.noreply.github.com>
2025-12-27 23:15:29 +0300

    верстка основной страницы с мобильным адаптивом и добавление карточек

---
+++ src/constants.js
@@ -0,0 +1,51 @@
+export const cardArray = [
+    {
+        id: 0,
+        title: 'Первый товар',
+        price: '170P',
+        address: ' Казань, р-н Вахитовский',
+        date: ' 10 июля 11:39',
+        img: './img/card-img.png' 
+    },
+     {
+        id: 1,
+        title: 'Второй товар',
+        price: '171P',
+        address: ' Казань, р-н Вахитовский',
+        date: ' 10 июля 11:39',
+        img: './img/card-img.png' 
+    },
+     {
+        id: 2,
+        title: 'Третий товар',
+        price: '172P',
+        address: ' Казань, р-н Вахитовский',
+        date: ' 10 июля 11:39',
+        img: './img/card-img.png' 
+    },
+     {
+        id: 3,
+        title: 'Четвертый товар',
+        price: '173P',
+        address: ' Казань, р-н Вахитовский',
+        date: ' 10 июля 11:39',
+        img: './img/card-img.png' 
+    },
+     {
+        id: 4,
+        title: 'Пятый товар',
+        price: '174P',
+        address: ' Казань, р-н Вахитовский',
+        date: ' 10 июля 11:39',
+        img: './img/card-img.png' 
+    },
+     {
+        id: 5,
+        title: 'Шестой товар',
+        price: '175P',
+        address: ' Казань, р-н Вахитовский',
+        date: ' 10 июля 11:39',
+        img: './img/card-img.png' 
+    }
+]
+export default cardArray;
